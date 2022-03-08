ca54d2e5e74de420b14c4dd5a883b21dee9609ab
fra <fra.sette.p.c@gmail.com>
2022-03-08 15:33:53 +0100

    fish

--- config.fish
+++ config.fish
@@ -5,18 +5,19 @@ end
 set fish_greeting
 
 export EDITOR=nvim
-export PATH=$PATH:~/.local/bin/scripts
+#export PATH=$PATH:~/.local/bin/scripts
+fish_add_path ~/.local/bin/scripts
 
-1='cd -1'
-2='cd -2'
-3='cd -3'
-4='cd -4'
-5='cd -5'
-6='cd -6'
-7='cd -7'
-8='cd -8'
-9='cd -9'
-_='sudo '
+alias 1='cd -1'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+#alias _='sudo '
 
 alias r=ranger
 alias t=tmux
