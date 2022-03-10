8dd8d70e94c1a3efc5aea7888cfcba7b12145b8f
fra <fra.sette.p.c@gmail.com>
2022-03-10 10:35:58 +0100

    fish col

--- config.fish
+++ config.fish
@@ -231,19 +231,24 @@ set -U fish_color_error c82829
 set -U fish_color_param 4271ae
 set -U fish_color_comment eab700
 set -U fish_color_match --background=brblue
-set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_selection cyan --bold --background=brblack
 set -U fish_color_search_match bryellow --background=brblack
 set -U fish_color_history_current --bold
 set -U fish_color_operator 00a6b2
 set -U fish_color_escape 00a6b2
 set -U fish_color_cwd green
 set -U fish_color_cwd_root red
-set -U fish_color_valid_path --underline
+# set -U fish_color_valid_path --underline
 set -U fish_color_autosuggestion 8e908c
 set -U fish_color_user brgreen
 set -U fish_color_host normal
 set -U fish_color_cancel -r
 set -U fish_pager_color_completion normal
 set -U fish_pager_color_description B3A06D yellow
-set -U fish_pager_color_prefix normal --bold --underline
-set -U fish_pager_color_progress brwhite --background=cyan
+# set -U fish_pager_color_prefix normal --bold --underline
+# set -U fish_pager_color_progress cyan --background=yellow
+# set fish_pager_color_description black
+# set fish_color_search_match --background=FF9940
+set fish_color_selection --background=black
+set fish_color_search_match --background=black
+# set fish_color_selection --background=FF9940
